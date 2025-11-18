39f72f2d12764ff00592904a003abc1e510ddeed
Trybukhouski <trybukhouskij@gmail.com>
2025-11-18 16:17:06 +0300

    feat: Render initial list with .map(). Closes #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { dummyData } from "./data/todos";
+
 function App() {
   return (
-    <main className="py-80">
-      <h1 className="font-bold">Your todo</h1>
+    <main>
+      <h1 className="font-bold text-center">Your todo</h1>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <p key={todo.id} className="text-lg">
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
